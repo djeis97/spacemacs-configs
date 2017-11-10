91ac26ffb977726c57b319573c056e6b4280c24b
Elijah Malaby <emalaby@digitalglobe.com>
2017-11-10 11:43:14 -0500

    Added roswell options to my lisp-implementations list

--- layers/djeis97-lisp/config.el
+++ layers/djeis97-lisp/config.el
@@ -1,2 +1,5 @@
+
 (defvar lisp-implementations '((sbcl-large ("sbcl" "--dynamic-space-size" "6144"))
-                               (sbcl ("sbcl"))))
+                               (sbcl ("sbcl"))
+                               (ros ("ros" "run"))
+                               (ros-local ("ros" "-S" "." "run"))))
