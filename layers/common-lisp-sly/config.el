36ad0c5d82c90aad09a9e02908899970669ff8e3
Elijah Malaby <qwe12345678910@gmail.com>
2017-12-09 19:32:10 -0500

    Fixed sly jump handler

--- layers/common-lisp-sly/config.el
+++ layers/common-lisp-sly/config.el
@@ -1,2 +1,2 @@
-(spacemacs|define-jump-handlers lisp-mode sly-edit-definition)
-(spacemacs|define-jump-handlers common-lisp-mode)
+
+(spacemacs|define-jump-handlers lisp-mode (sly-edit-definition :async t))
