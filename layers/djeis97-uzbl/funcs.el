9652953dd964f26aff3fb0ecc959feb9f3fdf848
Elijah Malaby <emalaby@digitalglobe.com>
2018-01-19 10:20:49 -0500

    Better handling for uzbl op

--- layers/djeis97-uzbl/funcs.el
+++ layers/djeis97-uzbl/funcs.el
@@ -1,15 +1,21 @@
 
 (defun djeis97-uzbl/uzbl-open-url (fifo urls &optional initial-input mess)
-  (with-temp-buffer
-    (insert-file-contents urls)
-    (let* ((candidates (swiper--candidates))
-           (url (ivy-read
-                 "Url: "
-                 (reverse candidates)
-                 :initial-input initial-input
-                 :caller 'djeis97-uzbl/uzbl-open-url))
-           (url (format "uri %s\n" (or (nth 3 (split-string url " ")) url))))
-      (write-region url nil
-                    fifo t 'dont))))
+  (unwind-protect
+      (with-temp-buffer
+        (insert-file-contents urls)
+        (let* ((candidates (swiper--candidates))
+               (url (ivy-read
+                     "Url: "
+                     (reverse candidates)
+                     :initial-input initial-input
+                     :caller 'djeis97-uzbl/uzbl-open-url))
+               (url (format "uri %s\n" (or (nth 3 (split-string url " ")) url))))
+          (write-region url nil
+                        fifo t 'dont)))
+    (djeis97-uzbl/reset-exwm-focus)))
+
+(defun djeis97-uzbl/reset-exwm-focus ()
+  (exwm-input--update-focus (selected-window))
+  (setq exwm-input--during-command nil))
 
 
