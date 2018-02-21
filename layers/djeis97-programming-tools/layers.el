e494cc56732890947522e3dca53b9a466d677189
Elijah Malaby <qwe12345678910@gmail.com>
2018-02-21 12:03:30 -0500

    Tweak autocomplete setup

--- layers/djeis97-programming-tools/layers.el
+++ layers/djeis97-programming-tools/layers.el
@@ -1,5 +1,7 @@
 (configuration-layer/declare-layers
  '((auto-completion :variables
+                    auto-completion-enable-help-tooltip t
+                    auto-completion-enable-sort-by-usage t
                     auto-completion-enable-snippets-in-popup t)
    (syntax-checking :variables
                     flycheck-gcc-language-standard "c++11")
