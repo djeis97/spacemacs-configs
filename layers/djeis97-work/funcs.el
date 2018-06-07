425467a53b9d30203aa6793293fabd6f5be1c88e
Elijah Malaby <emalaby@digitalglobe.com>
2018-06-07 11:07:38 -0400

    Rewrote display lock routine

--- layers/djeis97-work/funcs.el
+++ layers/djeis97-work/funcs.el
@@ -4,3 +4,7 @@
   (evil-insert-state)
   (smartparens-strict-mode))
 
+(defun djeis97-work/lock-display (message)
+  (interactive "s")
+  (start-process-shell-command "lock" nil (format "cinnamon-screensaver-command -l -m '%s 😸'" message)))
+
---
+++ layers/djeis97-work/keybindings.el
@@ -0,0 +1,5 @@
+
+(with-eval-after-load 'exwm
+  (exwm-input-set-key (kbd "s-l") #'djeis97-work/lock-display))
+
+
