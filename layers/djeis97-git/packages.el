baeedc91186253f7735968309d249b8c9828a13f
Elijah Malaby <qwe12345678910@gmail.com>
2018-06-11 16:11:01 -0400

    Builtin support for magithub now

--- layers/djeis97-git/packages.el
+++ layers/djeis97-git/packages.el
@@ -1,7 +1,6 @@
 
 (defconst djeis97-git-packages
   '(magit
-    (magithub :requires (magit))
     (magit-gh-pulls :excluded t)))
 
 (defun djeis97-git/pre-init-magit ()
@@ -10,8 +9,3 @@
     (magit-wip-after-apply-mode)
     (magit-wip-after-save-mode)))
 
-
-(defun djeis97-git/init-magithub ()
-  (use-package magithub
-    :after magit
-    :config (magithub-feature-autoinject t)))
